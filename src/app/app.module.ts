ba449d454515b44b96c9f5bb36c38120506ed624
Margaret Smith <mms0071@mix.wvu.edu>
2022-01-23 23:34:42 -0500

    Basis set up, adding navagation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TopNavbarComponent } from 'src/Navagation/topnavbar.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent
+
   ],
   imports: [
     BrowserModule
