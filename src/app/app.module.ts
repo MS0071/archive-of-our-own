8fcd4fd1b2714f617bbc8c22937ca1fcc6b9ec09
Margaret Smith <mms0071@mix.wvu.edu>
2022-01-25 15:27:51 -0500

    Put the second Navbar in a seperate folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TopNavbarComponent } from 'src/app/NavagationTop/topnavbar.component';
+import { TopNavbar2Component } from 'src/app/Navagation2/top2navbar.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavbarComponent
+    TopNavbarComponent,
+    TopNavbar2Component
 
   ],
   imports: [
