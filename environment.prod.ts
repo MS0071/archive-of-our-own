d58e92aeabf849e902e55804355c24f2b381200a
MS0071 <97578563+MS0071@users.noreply.github.com>
2022-03-22 12:28:48 -0400

    fixed the issues with the enviroments

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,13 +1,23 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 export const environment = {
   firebase: {
-    projectId: 'archive-of-our-own-dbb92',
-    appId: '1:912028900949:web:242f332cf6f786ffc17edc',
-    databaseURL: 'https://archive-of-our-own-dbb92-default-rtdb.firebaseio.com',
-    storageBucket: 'archive-of-our-own-dbb92.appspot.com',
-    apiKey: 'AIzaSyBEXoV7K8waUma7kozuhV5RHNOpKvqoKBo',
-    authDomain: 'archive-of-our-own-dbb92.firebaseapp.com',
-    messagingSenderId: '912028900949',
-    measurementId: 'G-ZMTL1ZQK25',
+    apiKey: "AIzaSyBEXoV7K8waUma7kozuhV5RHNOpKvqoKBo",
+  authDomain: "archive-of-our-own-dbb92.firebaseapp.com",
+  databaseURL: "https://archive-of-our-own-dbb92-default-rtdb.firebaseio.com",
+  projectId: "archive-of-our-own-dbb92",
+  storageBucket: "archive-of-our-own-dbb92.appspot.com",
+  messagingSenderId: "912028900949",
+  appId: "1:912028900949:web:242f332cf6f786ffc17edc",
+  measurementId: "G-ZMTL1ZQK25"
   },
+
+
+
+
+
+
   production: true
 };
--- environment.ts
+++ environment.ts
@@ -4,23 +4,14 @@
 
 export const environment = {
   firebase: {
-    projectId: 'archive-of-our-own-dbb92',
-    appId: '1:912028900949:web:242f332cf6f786ffc17edc',
-    databaseURL: 'https://archive-of-our-own-dbb92-default-rtdb.firebaseio.com',
-    storageBucket: 'archive-of-our-own-dbb92.appspot.com',
-    apiKey: 'AIzaSyBEXoV7K8waUma7kozuhV5RHNOpKvqoKBo',
-    authDomain: 'archive-of-our-own-dbb92.firebaseapp.com',
-    messagingSenderId: '912028900949',
-    measurementId: 'G-ZMTL1ZQK25',
+    apiKey: "AIzaSyBEXoV7K8waUma7kozuhV5RHNOpKvqoKBo",
+  authDomain: "archive-of-our-own-dbb92.firebaseapp.com",
+  databaseURL: "https://archive-of-our-own-dbb92-default-rtdb.firebaseio.com",
+  projectId: "archive-of-our-own-dbb92",
+  storageBucket: "archive-of-our-own-dbb92.appspot.com",
+  messagingSenderId: "912028900949",
+  appId: "1:912028900949:web:242f332cf6f786ffc17edc",
+  measurementId: "G-ZMTL1ZQK25"
   },
   production: false
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
